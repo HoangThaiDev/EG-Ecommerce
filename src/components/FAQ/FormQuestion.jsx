16931365c486fba5a8479438e50b224bdc8e9060
HoangThaiDev <thaindev2000@gmail.com>
2024-07-29 19:04:31 +0700

    Update Validate Form Component FAQs

--- src/components/FAQ/FormQuestion.jsx
+++ src/components/FAQ/FormQuestion.jsx
@@ -1,6 +1,7 @@
 // Import Modules
 import React from "react";
-
+import { useFormik } from "formik";
+import * as Yup from "yup";
 // Import File CSS
 import classes from "./css/formQuestion.module.css";
 
@@ -8,6 +9,26 @@ import classes from "./css/formQuestion.module.css";
 import { Row, Col } from "antd";
 
 function FormQuestion() {
+  // Create validateSchema by Yup
+  const FormQuestionSchema = Yup.object().shape({
+    name: Yup.string().required("Field name is required!"),
+    content: Yup.string()
+      .min(5, "Must be 5 characters or more")
+      .required("Field content is required!"),
+  });
+
+  // Create + use Hooks
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      content: "",
+    },
+    validationSchema: FormQuestionSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className={classes["introduce"]}>
       <Row className={classes["introduce__row"]}>
@@ -28,28 +49,53 @@ function FormQuestion() {
             <p>thaindev@gmail.com</p>
           </div>
         </Col>
-        <Col className={`${classes["introduce__col"]}`}>
-          <form className={classes["introduce__form"]}>
+        <Col className={classes["introduce__col"]}>
+          <div className={classes["introduce-card"]}>
             <h3>Ask any Question?</h3>
             <p>Your email address will not be published.</p>
             <p>Required fields are marked *</p>
+          </div>
 
+          <form
+            className={classes["introduce__form"]}
+            onSubmit={formik.handleSubmit}
+          >
             <div className={classes["form-input"]}>
               <label htmlFor="name">Your Name *</label>
               <input
                 className={classes["form-input-name"]}
                 type="text"
                 id="name"
+                name="name"
                 placeholder="Your Name"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                onBlur={formik.handleBlur}
               />
+              {formik.errors.name && formik.touched.name && (
+                <p className={classes["error-message-input"]}>
+                  Error: {formik.errors.name}
+                </p>
+              )}
+            </div>
+            <div className={classes["form-input"]}>
               <textarea
                 className={classes["form-input-text"]}
                 placeholder="Type your question"
+                name="content"
+                value={formik.values.content}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               ></textarea>
-              <button className={classes["form-btn-send"]} type="submit">
-                Send Now
-              </button>
+              {formik.errors.content && formik.touched.content && (
+                <p className={classes["error-message-input"]}>
+                  Error: {formik.errors.content}
+                </p>
+              )}
             </div>
+            <button className={classes["form-btn-send"]} type="submit">
+              Send Now
+            </button>
           </form>
         </Col>
       </Row>
